45e56365cd656a5fd801a74350815a55f33b1341
fukusy <fuku1study@gmail.com>
2021-09-18 14:53:43 +0000

    [Add]footerレイアウト変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :destroy]
+  resources :users, only: [:show, :index, :edit]
 
 end
