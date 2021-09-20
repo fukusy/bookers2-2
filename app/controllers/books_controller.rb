af534e76f1ab55acf19cbdbc70f26473c06a860b
fukusy <fuku1study@gmail.com>
2021-09-20 02:17:39 +0000

    [Add]loginを名前でできるように編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,27 @@
 class BooksController < ApplicationController
-  
+
   def index
-    @book = Book
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
