ac418289930c2729bc70846e1bd8286030466276
fukusy <fuku1study@gmail.com>
2021-09-21 04:40:19 +0000

    [Add]プロフィール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,32 @@ class UsersController < ApplicationController
   end
 
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+
   end
 
+
   def edit
+    @user_image = User.new
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def create
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :destroy]
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :create, :update]
 
 end
