a077fd9556625d2468bb20935382e2aecfaa1ac5
fukusy <fuku1study@gmail.com>
2021-09-27 13:50:33 +0000

    [Fix]同じページにリダイレクトでindexもいいね機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,16 +4,18 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    # ここにifでbook_pathかbooks_pathか判断する
-    redirect_to book_path(book)
+    # 同じページをリダイレクトする
+    redirect_to request.referer
+
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    # ここにifでbook_pathかbooks_pathか判断する
-    redirect_to book_path(book)
+    # 同じページをリダイレクトする
+    redirect_to request.referer
+
   end
 
 end
