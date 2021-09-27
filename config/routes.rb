7fbf4190dcf3ebf08b6440c6e67f784fb78935b5
fukusy <fuku1study@gmail.com>
2021-09-27 08:00:44 +0000

    [Add]Favoritesモデルとのアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get 'home/about'=> 'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :index, :edit, :update]
 
 end
