6c65a23d6462acd684b9f75ad6e4086c5972e532
fukusy <fuku1study@gmail.com>
2021-09-27 09:10:57 +0000

    [Add]本の詳細ページでいいね機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,10 +2,10 @@ class Book < ApplicationRecord
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  
-  def favoted_by?(user)
+  def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200}
