fb71a24e5152c9add8a3ad90d9abb00993d8021b
fukusy <fuku1study@gmail.com>
2021-10-02 06:17:16 +0000

    [Fix]フォロー機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,28 +13,29 @@ class User < ApplicationRecord
 
   # フォローの動き
   # フォローを取得。Relationshiopモデルのfollower_idにuser_idを格納
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follower_id",dependent: :destroy
   # following_userという変数にfollowerを通じて、相手のfollowedを確認して取得
-  has_many :following_user, through: :follower, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   # フォローワーの動き
   # フォローを取得。Relationshiopモデルのfollowed_idにuser_idを格納
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
-  # follower_userという変数にfollowedを通じて、相手のfollowerを確認して取得
-  has_many :follower_user, through: :followed, source: :follower
-  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 【class_name: "Relationship"】は省略可能
+  has_many :followings, through: :relationships, source: :followed
+
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
-    follower_user.include?(user)
+    followings.include?(user)
   end
-  
+
 
   attachment :profile_image
 
