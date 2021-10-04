5757f81d604aa35c79884723c0205d0f542ad0e0
fukusy <fuku1study@gmail.com>
2021-10-04 07:37:41 +0000

    [Add]検索機能の完全一致と部分一致のみ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'home/about'=> 'homes#about'
+  get '/search', to: 'searches#search'
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
