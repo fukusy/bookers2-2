a8a4b097f1996462c4f6ec0bffb8d9b2623759bf
fukusy <fuku1study@gmail.com>
2021-10-04 08:40:38 +0000

    [Finish]前方・後方検索機能追加完了とレイアウと変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,5 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
   
   def search
     @model = params["model"]
@@ -10,14 +11,22 @@ class SearchesController < ApplicationController
   private
   def search_for(model, content, method)
     if model == 'user'
-      if method == 'perfect'
+      if method == 'perfect'  # 完全一致検索
         User.where(name: content)
-      else
+      elsif method == 'front_match'   # 前方一致検索
+        User.where('name LIKE ?', content+'%')
+      elsif method == 'back_match'   # 後方一致検索
+        User.where('name LIKE ?', '%'+content)
+      else    # 部分一致検索
         User.where('name LIKE ?', '%'+content+'%')
       end
     elsif model == 'book'
       if method == 'perfect'
         Book.where(title: content)
+      elsif method == 'front_match'   # 前方一致検索
+        Book.where('name LIKE ?', content+'%')
+      elsif method == 'back_match'   # 後方一致検索
+        Book.where('name LIKE ?', '%'+content)
       else
         Book.where('title LIKE ?', '%'+content+'%')
       end
